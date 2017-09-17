6874df2603a0321d6fa3b46202dec1f545332f28
Mariel <marielbeth@gmail.com>
2017-09-17 21:16:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < Sinatra::Base
-
+  register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  enable :sessions
+
 end
--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,4 +2,29 @@ class FiguresController < ApplicationController
   get '/figures' do
     erb :'figures/index'
   end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      if !!Title.find_by(params[:title])
+        @figure.titles << Title.find_by(params[:title])
+      else
+        @figure.titles << Title.create(params[:title])
+      end
+    end
+    if !params[:landmark][:name].empty?
+      if !!Landmark.find_by(params[:landmark])
+        @figure.landmarks << Landmark.find_by(params[:landmark])
+      else
+        @figure.landmarks << Landmark.create(params[:landmark])
+      end
+    end
+
+    erb :'figures/index'
+  end
+
 end
